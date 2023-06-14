b1bec31274c1152a8d5a19c35740da46666c2f61
Anselmo Diabaca <anselmo23diababa@gmail.com>
2023-06-14 13:57:59 +0000

    Fix: Style

--- src/app/cronometro/cronometro.component.ts
+++ src/app/cronometro/cronometro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmailValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-cronometro',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./cronometro.component.css']
 })
 export class CronometroComponent {
+  nome: string = "Anselmo";
+  idade: number = 23;
+  telefone: number = 935149902;
 
+  hobe = ['Progamar', 'video Jogos', 'Cantar', 'Ler', 'Conduzir']
+
+  carro = {
+    nome: "RollRouce",
+    cor: "Preto e Branco",
+    rodas: "72Polegadas Cromada"
+  }
 }
