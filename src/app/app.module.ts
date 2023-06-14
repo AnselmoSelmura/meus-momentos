8e9292b2cff0d9aaaa8aa28ae03ca999c9c1e346
Anselmo Diabaca <anselmo23diababa@gmail.com>
2023-06-14 12:36:30 +0000

    Fix: novo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponentComponent } from './componentes/home-component/home.component';
-import { HomeComponent } from './home/home.component';
-
+import { AppComponent } from './app.component';import { HomeComponent } from './home/home.component';
+import { CronometroComponent } from './cronometro/cronometro.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    CronometroComponent,
 
   ],
   imports: [
